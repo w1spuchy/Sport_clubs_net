9afde79ae5fa1cd489a9e9e0070a79e5c7b12f8b
w1spuchy <a.shiroky2005@gmail.com>
2026-05-11 18:02:06 +0300

    get deleted endpoints for staff

--- server/routes/staff.router.js
+++ server/routes/staff.router.js
@@ -1,8 +1,28 @@
 import express from "express";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { createTrainer, createAdmin } from "../tableModels/staff.service.js";
+import { createTrainer, createAdmin, getEmployees, getTrainers, getAdmins, getEmployeeById, deleteEmployeeById } from "../tableModels/staff.service.js";
 
 const router = express.Router();
+
+router.get("/", asyncHandler(async (req, res) => {
+  const result = await getEmployees();
+  res.json(result);
+}));
+
+router.get("/trainers", asyncHandler(async (req, res) => {
+  const result = await getTrainers();
+  res.json(result);
+}));
+
+router.get("/admins", asyncHandler(async (req, res) => {
+  const result = await getAdmins();
+  res.json(result);
+}));
+
+router.get("/:id", asyncHandler(async (req, res) => {
+  const result = await getEmployeeById(req.params.id);
+  res.json(result);
+}));
 
 router.post("/trainers", asyncHandler(async (req, res) => {
   const result = await createTrainer(req.body);
@@ -13,5 +33,10 @@ router.post("/admins", asyncHandler(async (req, res) => {
   const result = await createAdmin(req.body);
   res.status(201).json(result);
 }));
+
+router.delete("/:id", asyncHandler(async (req, res) => {
+  const result = await deleteEmployeeById(req.params.id);
+  res.status(201).json(result);
+}))
 
 export default router;
--- server/tableModels/staff.service.js
+++ server/tableModels/staff.service.js
@@ -1,8 +1,72 @@
+import pool from "../dataBase.js";
 import { HttpError } from "../utils/HttpError.js";
 import { withTransaction } from "../utils/withTransaction.js";
 
-export async function createTrainer(payload) {
-  const { employee, trainer } = payload ?? {};
+export async function getEmployees() {
+    const [employees] = await pool.query(`
+        SELECT * FROM employees 
+        LEFT JOIN trainers USING(idEmployee)
+        LEFT JOIN admins USING(idEmployee)
+        ORDER BY idClub`
+    );
+    return employees;
+} 
+
+export async function getTrainers() {
+    const [trainers] = await pool.query(`
+        SELECT * FROM employees 
+        JOIN trainers USING(idEmployee)
+        ORDER BY idClub`
+    );
+    return trainers;
+} 
+
+export async function getAdmins() {
+    const [admins] = await pool.query(`
+        SELECT * FROM employees 
+        JOIN admins USING(idEmployee)
+        ORDER BY idClub`
+    );
+    return admins;
+} 
+
+export async function getEmployeeById(EmployeeId) {
+    const [employee] = await pool.query(`
+        SELECT * FROM employees 
+        LEFT JOIN trainers USING(idEmployee)
+        LEFT JOIN admins USING(idEmployee)
+        WHERE idEmployee = ?
+        ORDER BY idClub`, [EmployeeId]
+    );
+    if(!employee[0])
+    {
+      throw new HttpError(404, "No employee with this id");
+    }
+    return employee;
+}
+
+export async function deleteEmployeeById(EmployeeId) {
+    const [employee] = await pool.query(`
+        SELECT * FROM employees 
+        LEFT JOIN trainers USING(idEmployee)
+        LEFT JOIN admins USING(idEmployee)
+        WHERE idEmployee = ?
+        ORDER BY idClub`, [EmployeeId]
+    );
+
+    if(!employee[0])
+    {
+      throw new HttpError(404, "No employee with this id");
+    }
+
+    await pool.query(`
+      DELETE FROM employees WHERE idEmployee = ?`, [EmployeeId]);
+
+    return { employee, deleteStatus: "deleted"};
+}
+
+export async function createTrainer(body) {
+  const { employee, trainer } = body ?? {};
   if (!employee || !trainer) throw new HttpError(400, "employee and trainer are required");
 
   return withTransaction(async (conn) => {
@@ -66,8 +130,8 @@ export async function createTrainer(payload) {
   });
 }
 
-export async function createAdmin(payload) {
-  const { employee, admin } = payload ?? {};
+export async function createAdmin(body) {
+  const { employee, admin } = body ?? {};
   if (!employee || !admin) throw new HttpError(400, "employee and admin are required");
 
   return withTransaction(async (conn) => {
